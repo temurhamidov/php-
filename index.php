ab08c3cfd08620fecf86464c4946cb258d4fb1ce
temurhamidov <106656084+temurhamidov@users.noreply.github.com>
2022-06-05 23:36:22 -0700

    azaa

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+//  $a = "programm.iz";
+//    echo $a;
+
+//    $c = 23431234;
+//    echo $c;
+
+//    const PI = 3.14;
+//    echo defined("PI");
+
+
+//    defined("PIC", 2.14);
+//     echo defined("PIC");
+
+   //Arifmetik ammallar +, -, *, /, %, ., ++, --, =, += -=, *=, /=, %=, .=
+
+
+
+   // $a = 5;
+   // $b = 10;
+   // echo $a + $b;
+
+   // echo $b % $a;
+
+   // echo $b * $a;
+
+   // $a .= $b;
+   // echo $a;
+
+
+
+   //streln //strcmp //strpos
+
+   // echo strlen("progrmmm.uz");
+
+   // echo strcmp("welcome to", "welcome");
+
+   // echo strpos("welcom=to", "=");
+
+
+// strtolower //strtoupper
+
+   // echo strtolower("PROGGRAM");
+
+   // echo strtoupper("poijhgfdsd");
+
+
+
+   //trim //explode  //implode
+
+
+   $a = "B1234567";
+   echo trim($a, "B");
+
+
+   $key = "hello my favoriti country";
+   print_r(explode(",", $key));
+
+
+   $var = array("Hello", "Apple", "Microsoft");
+   print_r($var);
+
+   echo implode(" ", $var);
+
+
+?>
+
+
+
+
+
+
+
+
+
+
